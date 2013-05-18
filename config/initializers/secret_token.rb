eb35dfe28f234f7ddc3ec17f94a78b583fab52c2
thinkinggis_chris <thinkinggis_chris@mybox.(none)>
2013-05-18 03:55:12 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GlopitWeb::Application.config.secret_token = 'cbc0e333136df0393a10b23c246ee9b0067e7f61d3c5a847fc8602be76c8700e362f01a48a5d6a7b81cdd5063ef4ee589db1fbe1f4355f3bc16943ddc6e29a0d'
